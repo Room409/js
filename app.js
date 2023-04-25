b8214c27678ebb4a99cbc9be64b42822e0b82447
Martin Hilarion <martinh.sama@gmail.com>
2023-04-24 21:31:36 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+
+//Numbers
+let age = 20;
+let year = 2023;
+let pi = 3.1416;
+
+//String
+let name = "Martin";
+let lastName = "Hilarion";
+let course = "Programacion";
+
+//Booleans
+let day = false;
+let night = true;
+let experience = true;
+
+//null / undefined
+
+let cat = null;
+let lista;
+let nombres;
+
+//arrays
+
+let fruit = ["Manzana","Pera","mandarina","platano"]
+let countrys = ["Peru","Canada","Japon","USA","Colombia"]
+let pets = ["Gato","Perro","Loro"]
+let info = {nombre:'Martin', edad:"10"}
+
+

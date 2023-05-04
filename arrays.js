de1255c83a98899f4d00b4d7d45c2c6646051b6b
Martin Hilarion <martinh.sama@gmail.com>
2023-05-03 22:19:33 -0500

    arreglos

---
+++ arrays.js
@@ -0,0 +1,24 @@
+let persona = {
+    nombre: 'Martin',
+    apellido: 'Hilarion',
+    edad: '10',
+    genero: 'Masculino',
+    signo: 'Capricornio',
+}
+
+let mascota = {
+    nombre:'Kan',
+    tipo: 'Felino',
+    color: 'Naranja',
+    edad: '24 meses',
+    genero: 'Masculino'
+    
+}
+
+let personas = [
+    {nombre:'Martin', edad:'111', direccion:'Chorrillos 111'},
+    {nombre:'Kan', edad:'11', direccion:'Santa Anita 456'},
+    {nombre:'Abril', edad:'18', direccion:'Argentina 345'}
+]
+
+let arrayPlanetas = ['Jupiter','Urano','Pluton','Marte','Tierra']
--- horoscopo.js
+++ horoscopo.js
@@ -4,40 +4,40 @@ let mes = parseInt(prompt('Por favor ingresa el mes:'));
 
 let signo;
 
-if(mes === 3 && dia >= 21 || mes === 4 && dia <= 19){
+if (mes === 3 && dia >= 21 || mes === 4 && dia <= 19) {
     signo = 'Aries';
 }
-if(mes === 4 && dia >= 20 || mes === 5 && dia <= 20){
+if (mes === 4 && dia >= 20 || mes === 5 && dia <= 20) {
     signo = 'Tauro';
 }
-if(mes === 5 && dia >= 21 || mes === 6 && dia <= 20){
+if (mes === 5 && dia >= 21 || mes === 6 && dia <= 20) {
     signo = 'Géminis';
 }
-if(mes === 6 && dia >= 21 || mes === 7 && dia <= 22){
+if (mes === 6 && dia >= 21 || mes === 7 && dia <= 22) {
     signo = 'Cáncer';
 }
-if(mes === 7 && dia >= 23 || mes === 8 && dia <= 22){
+if (mes === 7 && dia >= 23 || mes === 8 && dia <= 22) {
     signo = 'Leo';
 }
-if(mes === 8 && dia >= 23 || mes === 9 && dia <= 22){
+if (mes === 8 && dia >= 23 || mes === 9 && dia <= 22) {
     signo = 'Virgo';
 }
-if(mes === 9 && dia >= 23 || mes === 10 && dia <= 22){
+if (mes === 9 && dia >= 23 || mes === 10 && dia <= 22) {
     signo = 'Libra';
 }
-if(mes === 10 && dia >= 23 || mes === 11 && dia <= 21){
+if (mes === 10 && dia >= 23 || mes === 11 && dia <= 21) {
     signo = 'Escorpio';
 }
-if(mes === 11 && dia >= 22 || mes === 12 && dia <= 21){
+if (mes === 11 && dia >= 22 || mes === 12 && dia <= 21) {
     signo = 'Sagitario';
 }
-if(mes === 12 && dia >= 22 || mes === 1 && dia <= 19){
+if (mes === 12 && dia >= 22 || mes === 1 && dia <= 19) {
     signo = 'Capricornio';
 }
-if(mes === 1 && dia >= 20 || mes === 2 && dia <= 18){
+if (mes === 1 && dia >= 20 || mes === 2 && dia <= 18) {
     signo = 'Acuario';
 }
-if(mes === 2 && dia >= 19 || mes === 3 && dia <= 20){
+if (mes === 2 && dia >= 19 || mes === 3 && dia <= 20) {
     signo = 'Piscis';
 }
 
